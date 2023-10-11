d1468304cf65c80b3c645409fde55ca3a367da14
Soulsaw <ssawadogo292@gmail.com>
2023-10-11 11:16:10 -0400

    add documentation to all function in module

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""
+"""This class defines all the commmon attributes/methods from others class
+
+Author: @DAVID & @SOULEYTECH
+Date: 10/10/2023
 """
 import uuid
 from datetime import datetime
@@ -7,10 +10,16 @@ from models import storage
 
 
 class BaseModel():
-    """
+    """This class defines all common attributes/methods for other class
+    This class does not inherit from another class
+    Date:10/10/2023
     """
     def __init__(self, *args, **kwargs):
-        """
+        """This is the method implemented the constructor
+
+        Args:
+            :param args(str): Is a non-keyworded arguments
+            :param kwargs(dict): Is a keyworded argument
         """
         size = len(kwargs)
         if size > 0:
@@ -26,20 +35,27 @@ class BaseModel():
             self.updated_at = datetime.now()
 
     def __str__(self):
-        """
+        """This fonction implement the __str__ method
+        :return (str): This method return a str representation of instance
         """
         return "[{}] ({}) {}" \
             .format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """
+        """This function permit to update an attribute with
+        the current datetime
+        This method return nothing, but call the new(self)
+        from the FileStorage class to add a new instance
         """
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """
+        """This function return the dictionary representation
+        of an instance of the class
+        :return (dict): Return a dictionnary representation of
+        an instance
         """
         my_list = self.__dict__
         my_list['__class__'] = self.__class__.__name__
