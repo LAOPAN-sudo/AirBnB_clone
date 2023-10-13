50ab64af791a407805ce90d0b17943dbcfbb5b27
Soulsaw <ssawadogo292@gmail.com>
2023-10-13 06:56:59 -0400

    fixing the create, show and all command and also the storage problem

--- console.py
+++ console.py
@@ -3,7 +3,14 @@
 Authors: @DAVID & @SOULEY
 """
 import cmd
+import re
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models import storage
 
 
@@ -13,8 +20,10 @@ class HBNBCommand(cmd.Cmd):
     to do some console
     """
     prompt = '(hbnb) '
+    classes = {
+        'BaseModel', 'Amenity', 'City', 'Place', 'Review', 'State', 'User'
+        }
 
-    instances = []
     def emptyline(self):
         """This method permit to override the default emptyline behavior
         """
@@ -38,82 +47,63 @@ class HBNBCommand(cmd.Cmd):
         if not arg:
             print('** class name missing **')
             return
-        if arg not in ['BaseModel']:
-            print("** class doesn't exist **")
+        if arg in self.classes:
+            obj = eval(arg)()
+            obj.save()
+            print(obj.id)
             return
-        if arg in ['BaseModel']:
-            my_model = BaseModel()
-            
-            new_list = self.instances+[my_model]
-            self.instances = new_list
-            print(my_model.id)
+        print("** class doesn't exist **")
+        return
 
     def do_show(self, arg):
-        """This command permit to show the content of an instance
-        of a BaseModel
+        """This command permit to create a new instance of BaseModel
         Args:
-            This method take a argument after the command
-        :param arg(str): split it in list of string
+            This command take one argument on parameter
+            :param arg(string): The string after the command
         """
         args = arg.split()
         if not arg:
             print('** class name missing **')
             return
-        if args[0] not in ['BaseModel']:
+        if args[0] not in self.classes:
             print("** class doesn't exist **")
             return
-        if args[0] in ['BaseModel'] and len(args) < 2:
-            print('** instance id missing **')
+        if len(args) < 2:
+            print("** id instance missing **")
             return
-        if len(self.instances) > 0:
-            for item in self.instances:
-                if item.id == args[1]:
-                    print(item)
-                    return
-            print('** no instance found **')
-            return
-        else:
-            print('** no instance found **')
-            return
+        all_objs = storage.all()
+        for obj_id in all_objs.keys():
+            if f"{args[0]}.{args[1]}" == obj_id:
+                obj = all_objs[obj_id]
+                print(obj)
+                return
+        print("** id instance not found **")
+        return
 
-    def do_destroy(self, arg):
-        """This command permit to show the content of an instance
-        of a BaseModel
+    def do_all(self, arg):
+        """This command permit to create a new instance of BaseModel
         Args:
-            This method take a argument after the command
-        :param arg(str): split it in list of string
+            This command take one argument on parameter
+            :param arg(string): The string after the command
         """
-        args = arg.split()
         if not arg:
-            print('** class name missing **')
+            all_objs = storage.all()
+            for obj_id in all_objs.keys():
+                obj = all_objs[obj_id]
+                print(obj)
             return
-        if args[0] not in ['BaseModel']:
-            print("** class doesn't exist **")
+        if arg in self.classes:
+            all_objs = storage.all()
+            for obj_id in all_objs.keys():
+                if re.search(arg, obj_id):
+                    obj = all_objs[obj_id]
+                    print(obj)
             return
-        if args[0] in ['BaseModel'] and len(args) < 2:
-            print('** instance id missing **')
-            return
-        if len(self.instances) > 0:
-            for item in self.instances:
-                if item.id == args[1]:
-                    del item
-        storage.reload()
-        allobjs = storage.all()
-        b = True
-        for key in allobjs.keys():
-            if f"{'BaseModel'}.{args[1]}" == key:
-                b = False
-        if b == False:
-            print('OK')
-            del allobjs[f"{'BaseModel'}.{args[1]}"]
-            
-
-
-
+        print("** class doesn't exist **")
+        return
 
     """Implementation of helping
     """
-    
     def help_quit(self):
         """Give the doc of the quit command
         """
