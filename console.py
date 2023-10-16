cd6665fc94c7730dd9080e2f4b97c3a5cce53f4c
Soulsaw <ssawadogo292@gmail.com>
2023-10-16 18:39:53 -0400

    fix the pycodestyle error and the update commande error

--- console.py
+++ console.py
@@ -44,6 +44,7 @@ class HBNBCommand(cmd.Cmd):
                 match1 = re.search(r"(\(.*\))", arg)
                 if match1:
                     argss = match1.group()[1:-1]
+                    print("OK",argss)
                     argt = args[0]
                     if argss != '':
                         argss = argss.split(', ')
